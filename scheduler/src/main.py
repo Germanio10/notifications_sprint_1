870398b7c8027be612d97e10cbfbaf916325b32d
mrrobot <mahaon_1990@mail.ru>
2024-05-02 20:07:56 +0300

    добавлен генератор

---
+++ scheduler/src/main.py
@@ -0,0 +1,32 @@
+import asyncio
+import contextlib
+
+from utils.fake_user import User
+from connection.conn import connection
+from core.config import settings
+from services.scheduler import Scheduler
+from services.scheduler_job import SchedulerJob
+
+
+async def main() -> None:
+    db, broker = await connection()
+
+    user = User()
+    scheduler_job = SchedulerJob(broker.send_to_broker)
+
+    scheduler = Scheduler(db, scheduler_job, user)
+    await scheduler.download_scheduled_notifications()
+
+    await broker.consume(settings.queue_scheduled, scheduler.incoming)
+    await broker.consume(settings.queue_remove_scheduled, scheduler.remove)
+
+    try:
+        await asyncio.Future()
+    finally:
+        await db.close()
+        await broker.close()
+
+
+if __name__ == '__main__':
+    with contextlib.suppress(KeyboardInterrupt, SystemExit):
+        asyncio.run(main())
