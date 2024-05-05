55522a485d3608b93c5145d6760664d41764bf26
mrrobot <mahaon_1990@mail.ru>
2024-05-05 15:39:06 +0300

    фикс ревью

--- event_api/src/core/config.py
+++ event_api/src/core/config.py
@@ -3,18 +3,18 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    project_name: str = Field(validation_alias='PROJECT_NAME_', default='notifications')
-    debug: bool = Field(validation_alias='DEBUG_', default=True)
+    project_name: str = Field(validation_alias='PROJECT_NAME', default='notifications')
+    debug: bool = Field(validation_alias='DEBUG', default=True)
 
-    mongo_uri: str = Field(validation_alias='MONGO_URI_', default='mongodb://127.0.0.1:27017/')
-    mongo_db: str = Field(validation_alias='MONGO_DB_', default='notifications')
+    mongo_uri: str = Field(validation_alias='MONGO_URI', default='mongodb://127.0.0.1:27017/')
+    mongo_db: str = Field(validation_alias='MONGO_DB', default='notifications')
 
     rabbit_uri: str = Field(
-        validation_alias='RABBIT_URI_', default='amqp://admin:P@ssw0rd@127.0.0.1:5672/'
+        validation_alias='RABBIT_URI', default='amqp://admin:P@ssw0rd@127.0.0.1:5672/'
     )
-    queue_instant: str = Field(validation_alias='QUEUE_INSTANT_', default='instant.notification')
+    queue_instant: str = Field(validation_alias='QUEUE_INSTANT', default='instant.notification')
     queue_scheduled: str = Field(
-        validation_alias='QUEUE_SCHEDULED_', default='scheduled.notification'
+        validation_alias='QUEUE_SCHEDULED', default='scheduled.notification'
     )
 
 
